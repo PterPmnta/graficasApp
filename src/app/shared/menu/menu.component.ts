ec11b5cef820c75c55d4634cf62a5d9e72ae46c6
pterpmnta <pedro.pimienta@cargologik.com>
2022-08-13 20:40:11 -0500

    setting menu and dinamyc routes

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
-  ]
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MenuComponent {
+  menu: MenuItem[] = [
+    {
+      ruta: '/graficas/barra',
+      texto: 'Barras',
+    },
+    {
+      ruta: '/graficas/barra-doble',
+      texto: 'Barra Doble',
+    },
+    {
+      ruta: '/graficas/dona',
+      texto: 'Dona',
+    },
+    {
+      ruta: '/graficas/dona-api',
+      texto: 'Dona API',
+    },
+  ];
 }

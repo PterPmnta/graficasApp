aa2c8716f6cdd42542c537872f96e2af3c98cd75
pterpmnta <pedro.pimienta@cargologik.com>
2022-08-15 19:48:22 -0500

    graphic bar component modified

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,16 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-grafica-barra',
   templateUrl: './grafica-barra.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class GraficaBarraComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public barChartType: ChartType = 'bar';
 
-  constructor() { }
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      { data: [10, 25, 50, 91, 24, 41, 66], label: 'Series C' },
+    ],
+  };
 
-  ngOnInit(): void {
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    this.barChartData.datasets.forEach((item) => {
+      item.data.map((valor, indice) => {
+        console.log(valor);
+        item.data[indice] = Math.round(Math.random() * 100);
+      });
+    });
+
+    this.chart?.update();
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }

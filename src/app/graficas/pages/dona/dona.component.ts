c4941e819769b1734cf2b75d50f0a0ece0dc99cf
pterpmnta <pedro.pimienta@cargologik.com>
2022-08-16 20:52:49 -0500

    chart dona component completed

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,16 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: ['#c9ada1', '#EAE0CC', '#F8D210'],
+      },
+      {
+        data: [50, 150, 120],
+        backgroundColor: ['#6405F0', '#0724E3', '#05A0F0'],
+      },
+      {
+        data: [250, 130, 70],
+        backgroundColor: ['#FF8300', '#B7AC44', '#FF4500'],
+      },
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: {}[];
+  }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: {}[];
+  }): void {
+    console.log(event, active);
+  }
 }
